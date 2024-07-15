368361230479a26b440961a00369947edbbf4040
Casey Echternacht <casechtern@yahoo.com>
2024-07-15 13:55:02 -0600

    simplified

--- src/main/java/org/example/Problem1.java
+++ src/main/java/org/example/Problem1.java
@@ -1,2 +1,82 @@
-package org.example;public class Problem1 {
+package org.example;
+
+import java.util.Scanner;
+
+public class Problem1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+//		Problem1(5);
+//		Problem2(5);
+//		Problem3(15);
+//		Problem4(20);
+//		Problem5();
+//		Problem6();
+
+	}
+
+	public static void Problem1(int x) {
+		if (x <= 10) System.out.println("Less than 10");
+	}
+	public static void Problem2(int x) {
+		if (x < 10) System.out.println("Less than 10");
+		else if (x > 10) System.out.println("Greater than 10");
+	}
+	public static void Problem3(int x) {
+		if (x < 10) System.out.println("Less than 10");
+		else if (x > 10 && x < 20) System.out.println("Greater than 20");
+		else System.out.println("Greater than or equal to 20");
+	}
+	public static void Problem4(int x) {
+		if (x < 10 || x > 20) System.out.println("Out of range");
+		else System.out.println("In range");
+	}
+	public static void Problem5() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What score did you get?");
+		int score = scanner.nextInt();
+		scanner.close();
+
+		if (score > 100 || score < 0) System.out.println("Score out of range");
+		else if (score > 90) System.out.println("A");
+		else if (score > 80) System.out.println("B");
+		else if (score > 70) System.out.println("C");
+		else if (score > 60) System.out.println("D");
+		else System.out.println("F");
+	}
+	public static void Problem6() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Pick a number from 1-7:");
+		int input = scanner.nextInt();
+		scanner.close();
+
+		switch (input) {
+			case 1:
+				System.out.println("Sunday");
+				break;
+			case 2:
+				System.out.println("Monday");
+				break;
+			case 3:
+				System.out.println("Tuesday");
+				break;
+			case 4:
+				System.out.println("Wednesday");
+				break;
+			case 5:
+				System.out.println("Thursday");
+				break;
+			case 6:
+				System.out.println("Friday");
+				break;
+			case 7:
+				System.out.println("Saturday");
+				break;
+			default: System.out.println("Out of range");
+
+		}
+	}
+
+
 }
+
--- src/main/java/org/example/TaxCalculator.java
+++ src/main/java/org/example/TaxCalculator.java
@@ -1,2 +1,61 @@
-package org.example;public class TaxCalculator {
+package org.example;
+
+import java.util.Scanner;
+
+public class TaxCalculator {
+	public static void main(String[] args) {
+		int status = getStatus();
+		double income = getIncome();
+
+		final int[][] brackets = {
+			{8350, 33950, 82250, 171550, 372950}, // Single
+			{16700, 67900, 137050, 208850, 372950}, // Married Filing Jointly
+			{8350, 33950, 68525, 104425, 186475}, // Married Filing Separately
+			{11950, 45500, 117450, 190200, 372950} // Head of Household
+		};
+		final double[] rates = {0.10, 0.15, 0.25, 0.28, 0.33, 0.35};
+
+		double tax = calculateTax(status, income, brackets, rates);
+		System.out.printf("Tax is: $%.2f%n", tax);
+
+	}
+
+	private static double calculateTax(int status, double income, int[][] brackets, double[] rates) {
+		double tax = 0;
+		int[] bracket = brackets[status];
+
+		for (int i = 0; i < brackets.length; i++) {
+			if (income < bracket[i]) {
+				tax += income * rates[i];
+				return tax;
+			} else {
+				tax += bracket[i] * rates[i];
+				income -= bracket[i];
+			}
+		}
+			tax += income * rates[rates.length - 1];
+			return tax;
+	}
+
+
+	public static double getIncome() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your income:");
+		return input.nextDouble();
+	}
+	public static int getStatus() {
+		int status = -1;
+		do {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter your filing status:");
+			System.out.println("0 : Single");
+			System.out.println("1 : Married filing jointly or qualified widow");
+			System.out.println("2 : Married filing separately");
+			System.out.println("3 : Head of household");
+			status = input.nextInt();
+			if (status < 0 || status > 3) System.out.println("Invalid status");
+		} while (status < 0 || status > 3);
+		return status;
+	}
+
 }
